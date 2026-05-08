a48fef601c614358b0c3fee897bb7d69b78b3b9f
davi <davi.xa.oliveira@gmail.com>
2026-05-08 20:18:54 -0300

    exibe uma mensagem diferente para menor e maior de idade

---
+++ ativ-idade/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("digite sua idade:");
+int idade = Convert.ToInt32(Console.ReadLine()); 
+
+if (idade >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("voce é maior de idade");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("voce é menor de idade");
+}
+Console.ResetColor();
+Console.WriteLine("obrigado");
